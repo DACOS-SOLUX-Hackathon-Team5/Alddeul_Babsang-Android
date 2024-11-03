623e40ceda3797511bc490f58c7c7a83122878df
gaeulzzang <gaeulzzang@gmail.com>
2024-11-04 01:19:50 +0900

    #1 [FEAT] 기본 백그라운드 흰색으로 설정

--- app/src/main/java/com/hackathon/alddeul_babsang/presentation/main/screen/MainActivity.kt
+++ app/src/main/java/com/hackathon/alddeul_babsang/presentation/main/screen/MainActivity.kt
@@ -10,14 +10,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.hackathon.alddeul_babsang.core_ui.theme.AlddeulBabsangTheme
 import com.hackathon.alddeul_babsang.presentation.babsang.navigation.BabsangNavigator
 import com.hackathon.alddeul_babsang.presentation.example.navigation.ExampleNavigator
@@ -40,6 +46,8 @@ class MainActivity : ComponentActivity() {
                 val context = LocalContext.current
                 var backPressedState by remember { mutableStateOf(true) }
                 var backPressedTime = 0L
+                val systemUiController = rememberSystemUiController()
+                val lifecycleOwner = LocalLifecycleOwner.current
 
                 BackHandler(enabled = backPressedState) {
                     if (System.currentTimeMillis() - backPressedTime <= 3000) {
@@ -49,6 +57,20 @@ class MainActivity : ComponentActivity() {
                         context.toast("한번 더 누르면 종료돼요")
                     }
                     backPressedTime = System.currentTimeMillis()
+                }
+
+                SideEffect {
+                    systemUiController.setStatusBarColor(
+                        color = White
+                    )
+                }
+
+                DisposableEffect(key1 = lifecycleOwner) {
+                    onDispose {
+                        systemUiController.setStatusBarColor(
+                            color = Color.Transparent
+                        )
+                    }
                 }
 
                 val navController = rememberNavController()

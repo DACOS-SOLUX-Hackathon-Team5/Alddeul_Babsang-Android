b07fbcc5d1cc6d01e41559c9ede33164687ea23c
gaeulzzang <gaeulzzang@gmail.com>
2024-11-05 14:10:46 +0900

    #9 [REFACTOR] 회원가입1 페이지 리팩토링

--- app/src/main/java/com/hackathon/alddeul_babsang/presentation/auth/SignUpScreen1.kt
+++ app/src/main/java/com/hackathon/alddeul_babsang/presentation/auth/SignUpScreen1.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,19 +26,14 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.hackathon.alddeul_babsang.R
 import com.hackathon.alddeul_babsang.core_ui.component.AlddeulButton
-import com.hackathon.alddeul_babsang.core_ui.component.SignUpLoginTextField
+import com.hackathon.alddeul_babsang.core_ui.component.AuthTextField
 import com.hackathon.alddeul_babsang.core_ui.theme.AlddeulBabsangTheme
 import com.hackathon.alddeul_babsang.core_ui.theme.head4Bold
 
 @Composable
 fun SignUpScreen1(navController: NavController) {
-    val (id, setId) = remember {
-        mutableStateOf("")
-    }
-
-    val (password, setPassword) = remember {
-        mutableStateOf("")
-    }
+    var id by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
@@ -48,41 +45,33 @@ fun SignUpScreen1(navController: NavController) {
         verticalArrangement = Arrangement.Top
     ) {
         Text(
-            stringResource(R.string.setIdPassword),
+            stringResource(R.string.tv_signup_setIdPassword),
             textAlign = TextAlign.Center,
             style = head4Bold,
             lineHeight = 25.sp
-
-            )
+        )
         Spacer(modifier = Modifier.height(50.dp))
-
-        SignUpLoginTextField(
+        AuthTextField(
             value = id,
-            onValueChange = setId,
-            placeholderText = stringResource(R.string.setId)
+            onValueChange = { id = it },
+            placeholderText = stringResource(R.string.tf_signup_setId)
         )
-
         Spacer(modifier = Modifier.height(16.dp))
-
-        SignUpLoginTextField(
+        AuthTextField(
             value = password,
-            onValueChange = setPassword,
-            placeholderText = stringResource(R.string.setPassword)
+            onValueChange = { password = it },
+            placeholderText = stringResource(R.string.tf_signup_setPassword)
         )
-
         Spacer(modifier = Modifier.weight(1f))
-
         AlddeulButton(
-            text = R.string.next,
+            text = R.string.btn_signup_next,
             onClick = {
                 if (id.isNotEmpty() && password.isNotEmpty()) {
                     //navController.navigate("signUp2")
                 }
             }
         )
-
     }
-
 }
 
 
--- app/src/main/java/com/hackathon/alddeul_babsang/presentation/report/screen/ReportScreen.kt
+++ app/src/main/java/com/hackathon/alddeul_babsang/presentation/report/screen/ReportScreen.kt
@@ -3,11 +3,13 @@ package com.hackathon.alddeul_babsang.presentation.report.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.hackathon.alddeul_babsang.presentation.report.navigation.ReportNavigator
 
 @Composable
@@ -30,7 +32,10 @@ fun ReportScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "제보")
+        Text(
+            modifier = Modifier.padding(bottom = 10.dp),
+            text = "제보"
+        )
         Button(onClick = { onReportWriteClick() }) {
             Text(text = "제보하러 가기")
         }
